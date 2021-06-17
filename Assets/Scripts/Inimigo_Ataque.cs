5ed6ed202bc8f3ee03f23a58ad68a4a8ce062736
EduardoMineiroOliveira <82737909+EduardoMineiroOliveira@users.noreply.github.com>
2021-06-16 21:08:26 -0300

    Add files via upload

---
+++ Assets/Scripts/Inimigo_Ataque.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Inimigo_Ataque : MonoBehaviour
+{
+    public Slider vidaPlayer;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            vidaPlayer.value--;
+        }
+    }
+
+    private void Update()
+    {
+        if (vidaPlayer.value == 0)
+        {
+
+            SceneManager.LoadScene("GameOver");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 12);
+            //player.SetActive(false);
+        }
+    }
+
+
+}
