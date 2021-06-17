c3e70ebbbe9bab573fc69df64ad172b429336dcd
EduardoMineiroOliveira <82737909+EduardoMineiroOliveira@users.noreply.github.com>
2021-06-16 21:12:41 -0300

    Add files via upload

---
+++ Assets/Scripts/TelaStartGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TelaStartGame : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.tag == "Player")
+        {
+            Debug.Log("CARREGANDO TelaFinal...");
+            SceneManager.LoadScene("Loading_StartGame");
+        }
+
+
+    }
+
+}
+
